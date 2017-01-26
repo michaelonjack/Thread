d71e91f68772f5d343c5133b486889b6d8dc8b6c
Michael Onjack <mikeonjack@gmail.com>
2017-01-25 21:59:08 -0500

    Adds the ability to retrieve data from the database and fill the fields when the view loads

--- Thread/ClothingItemViewController.swift
+++ Thread/ClothingItemViewController.swift
@@ -22,6 +22,7 @@ class ClothingItemViewController: UIViewController, UIImagePickerControllerDeleg
     let currentUserClothingImagesRef = FIRStorage.storage().reference(withPath: "images/" + (FIRAuth.auth()?.currentUser?.uid)!)
     
     var clothingType: ClothingType!
+    var imageDidChange: Bool! = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,6 +39,8 @@ class ClothingItemViewController: UIViewController, UIImagePickerControllerDeleg
             case .Accessories:
                 labelViewTitle.text = "accessories"
         }
+        
+        loadUserData()
     }
 
     override func didReceiveMemoryWarning() {
@@ -45,38 +48,54 @@ class ClothingItemViewController: UIViewController, UIImagePickerControllerDeleg
         // Dispose of any resources that can be recreated.
     }
     
-    // Action to handle when the user wants to save the data in the view
+    
+    /*
+        Action to handle when the user wants to save the data in the view
+    */
     @IBAction func saveDidTouch(_ sender: Any) {
         let clothingItemName = textFieldItemName.text
         let clothingItemBrand = textFieldItemBrand.text
         let clothingItemLink = textFieldItemLink.text
         
+        // Create a new ClothingItem object to represent the new data
         let newClothingItem = ClothingItem(name: clothingItemName!, type: clothingType, brand: clothingItemBrand!, itemUrl: clothingItemLink!)
         
+        // Add the data to the database for the current user
         let currentUserClothingTypeRef = currentUserRef.child(clothingType.description)
         currentUserClothingTypeRef.setValue(newClothingItem.toAnyObject())
         
+        // Enter the user's image into the database
         if imageViewClothingPicture.image != nil {
             let imageMetaData = FIRStorageMetadata()
             imageMetaData.contentType = "image/png"
         
+            // Create a Data object to represent the image as a PNG
             var imageData = Data()
             imageData = UIImagePNGRepresentation(imageViewClothingPicture.image!)!
         
+            // Get reference to the user's clothing type in Firebase Storage
             let currentUserClothingTypeImagesRef = currentUserClothingImagesRef.child(clothingType.description)
-            currentUserClothingTypeImagesRef.put(imageData, metadata: imageMetaData) { (metaData, error) in
-                if error == nil {
-                    let downloadUrl = metaData?.downloadURL()?.absoluteString
-                    currentUserClothingTypeRef.updateChildValues(["pictureUrl": downloadUrl!])
-                } else {
-                    print(error?.localizedDescription ?? "Error uploading data to storage")
+            
+            // Add the image to Firebase Storage if it has changed
+            if imageDidChange == true {
+                currentUserClothingTypeImagesRef.put(imageData, metadata: imageMetaData) { (metaData, error) in
+                    if error == nil {
+                        // Add the image's url to the Firebase database
+                        let downloadUrl = metaData?.downloadURL()?.absoluteString
+                        currentUserClothingTypeRef.updateChildValues(["pictureUrl": downloadUrl!])
+                    } else {
+                        print(error?.localizedDescription ?? "Error uploading data to storage")
+                    }
                 }
             }
         }
     }
     
-    // Action to handle when the user selects the camera button
-    // Uses the user's camera to take a picture
+    
+    /*
+        Action to handle when the user selects the camera button
+        Uses the user's camera to take a picture
+    */
     @IBAction func cameraDidTouch(_ sender: Any) {
         if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera) {
             picker.sourceType = UIImagePickerControllerSourceType.camera;
@@ -87,8 +106,11 @@ class ClothingItemViewController: UIViewController, UIImagePickerControllerDeleg
         }
     }
     
-    // Action to handle when the user selects the photo library
-    // Allows the user to select a photo from their phone library
+    
+    /*
+        Action to handle when the user selects the photo library button
+        Allows the user to select a photo from their phone library
+    */
     @IBAction func photoLibraryDidTouch(_ sender: Any) {
         picker.allowsEditing = false
         picker.sourceType = .photoLibrary
@@ -96,7 +118,11 @@ class ClothingItemViewController: UIViewController, UIImagePickerControllerDeleg
         present(picker, animated: true, completion: nil)
     }
     
-    // Sets the select image as the ImageView's image
+    
+    /*
+        Pulls the image chosen by the user (via their photo library or camera) and sets that as the view's image
+        Called by cameraDidTouch and photoLibraryDidTouch
+    */
     func imagePickerController(_ picker: UIImagePickerController,
                                didFinishPickingMediaWithInfo info: [String : AnyObject])
     {
@@ -104,19 +130,36 @@ class ClothingItemViewController: UIViewController, UIImagePickerControllerDeleg
         imageViewClothingPicture.contentMode = .scaleAspectFit
         imageViewClothingPicture.image = chosenImage
         dismiss(animated:true, completion: nil)
+        self.imageDidChange = true
     }
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         dismiss(animated: true, completion: nil)
     }
-
+    
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
+        Retrieves the current user's data from the database and fills the view fields
+        Called when view loads
     */
+    func loadUserData() {
+        
+        // Load the stored image
+        currentUserRef.child(clothingType.description).observeSingleEvent(of: .value, with: { (snapshot) in
+            
+            let storedData = snapshot.value as? NSDictionary
+            
+            self.textFieldItemName.text = storedData?["name"] as? String ?? ""
+            self.textFieldItemBrand.text = storedData?["brand"] as? String ?? ""
+            self.textFieldItemLink.text = storedData?["link"] as? String ?? ""
+            
+            if snapshot.hasChild("pictureUrl") {
+                self.currentUserClothingImagesRef.child(self.clothingType.description).data(withMaxSize: 20*1024*1024, completion: {(data, error) in
+                    let clothingImage = UIImage(data:data!)
+                    self.imageViewClothingPicture.image = clothingImage
+                })
+            }
+        })
+    }
+
 
 }
