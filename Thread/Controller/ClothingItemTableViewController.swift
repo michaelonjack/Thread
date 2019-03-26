08286afa5666dfef2e544526feff00136b650d04
Michael Onjack <mikeonjack@gmail.com>
2019-03-25 21:50:44 -0400

    Adds ability to favorite and unfavorite items in the Favorites table view

--- Thread/Controller/ClothingItemTableViewController.swift
+++ Thread/Controller/ClothingItemTableViewController.swift
@@ -98,4 +98,33 @@ extension ClothingItemTableViewController: ClothingItemTableCellDelegate {
             }
         }
     }
+    
+    func favoriteItem(at cell: ClothingItemTableViewCell) {
+        guard let indexPath = clothingItemTableView.indexPath(for: cell) else { return }
+        guard let currentUser = configuration.currentUser else { return }
+        
+        let currentItem = clothingItems[indexPath.row]
+        
+        // User currently has the item in their favorites so they're un-favoriting the item
+        if currentUser.favoritedItems.contains(currentItem) {
+            currentUser.favoritedItems.removeAll(where: { $0 == currentItem })
+            
+            cell.favoriteButton.isSelected = false
+        }
+            
+            // User is requesting to add the item to their favorites
+        else {
+            currentUser.favoritedItems.append(currentItem)
+            
+            cell.favoriteButton.isSelected = true
+            
+            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseIn, animations: {
+                cell.favoriteButton.transform = cell.favoriteButton.transform.scaledBy(x: 2, y: 2)
+            }) { (_) in
+                UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+                    cell.favoriteButton.transform = cell.favoriteButton.transform.scaledBy(x: 0.5, y: 0.5)
+                })
+            }
+        }
+    }
 }
