26ee82cb75fa67243545792cab414ab2c5fd3129
Michael Onjack <mikeonjack@gmail.com>
2019-04-23 17:37:42 -0400

    Only shows items in the Explore view that have an image saved

--- Thread/Controller/Home/HomeViewController+UICollectionView.swift
+++ Thread/Controller/Home/HomeViewController+UICollectionView.swift
@@ -30,7 +30,7 @@ extension HomeViewController: UICollectionViewDelegate {
             let location = configuration.places[indexPath.row]
             let locationExpandedView = ExploreLocationExpandedView(frame: frameInRootView)
             locationExpandedView.translatesAutoresizingMaskIntoConstraints = false
-            locationExpandedView.detailsView.nearbyItems = location.nearbyItems
+            locationExpandedView.detailsView.nearbyItems = location.nearbyItems.filter { $0.1.itemImageUrl != nil || $0.1.smallItemImageUrl != nil }
             locationExpandedView.locationImageView.image = selectedCell.imageView.image
             locationExpandedView.detailsView.nameLabel.text = location.name
             
